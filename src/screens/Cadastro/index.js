56e3583de9035266bec04f1bc96f233ba0e9ae6e
NCristofe <cristofegit@gmail.com>
2026-08-25 10:55:58 -0300

    feat: adiciona tela de Cadastro

---
+++ src/screens/Cadastro/index.js
@@ -0,0 +1,13 @@
+import { Image, Pressable, Text, TextInput, View } from 'react-native'
+import { styles } from './style'
+import logo from '../../assets/logoShopSwift.png'
+
+
+export const Cadastro = () => {
+
+    return (
+        <View>
+
+        </View>
+    )
+}
