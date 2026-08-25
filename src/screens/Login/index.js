74fca2411cd6a6ba6daa6e9b787b29cb290a7fbe
NCristofe <cristofegit@gmail.com>
2026-08-25 10:55:53 -0300

    feat: adiciona formulario de login com campos de email e senha

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,14 +1,44 @@
-import { Image, Text, View } from 'react-native'
+import { Image, Pressable, Text, TextInput, View } from 'react-native'
 import { styles } from './style'
 import logo from '../../assets/logoShopSwift.png'
 
 export const Login = () => {
-    return(
+    return (
 
-        <View>
+        <View style={styles.containerHome}>
             <Image source={logo}/>
 
-            <Text style={styles.txtTitulo}>Faça login e se <Text style={styles.txtHighlights}>surpreenda</Text></Text>
+            <View style={styles.contentHome}>
+
+                <View>
+
+                    <Text style={styles.txtTitulo}>Faça login e se <Text style={styles.txtHighlights}>surpreenda</Text></Text>
+                    <View style={styles.containerForm}>
+
+                        <View style={styles.containerInputs}>
+
+                            <View>
+                                <Text>E-mail</Text>
+                                <TextInput style={styles.Input} keyboardType='email-address' />
+                            </View>
+
+                            <View>
+                                <Text>Senha</Text>
+                                <TextInput style={styles.Input} secureTextEntry={true}/>
+                            </View>
+
+                        </View>
+
+                        <Pressable style={styles.buttonLogin}>
+                            <Text style={styles.txtLogin}>Entrar</Text>
+                        </Pressable>
+
+                    </View>
+
+                    <Text style={styles.txtNaoTemConta}> Não tem um conta? <Text style={styles.txtCriarConta}>Criar conta</Text></Text>
+                </View>
+
+            </View>
 
         </View>
 
--- src/screens/Login/style.js
+++ src/screens/Login/style.js
@@ -1,8 +1,68 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    txtTitulo:{
+    containerHome:{
+
+    gap: 110,
+    paddingHorizotal: 20,
+
+    },
+
+    contentHome:{
+        gap:40
+
+    },
+
+    containerForm:{
+        gap:40
+    },
+
+    containerInputs:{
+        gap:25
+    },
+
+    Input:{
+        borderWidth: 1,
+        borderColor: "#2d3133",
+        borderRadius: 7,
+        padding: 10
+        
+    },
+
+    buttonLogin:{
+        backgroundColor: "#375EE7",
+        borderRadius: 7,
+        paddingVertical: 15
+
+    },
+    
+
+    txtLogin:{
+        color: "#fff",
+        fontWeight: "bold",
+        textAlign: "center",
+
+
+    },
+
+    txtNaoTemConta:{
         color: "#2D3133",
+        textAlign: "center"
+    },
+
+    txtCriarConta:{
+        color: "#375EE7",
+        fontWeight: "bold"
+    },
+
+
+
+
+        
+
+
+    txtTitulo:{
+       color: "#2D3133",
         fontSize: 40,
         fontWeight: 700
     },
