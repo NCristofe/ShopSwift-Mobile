0872c57401a2ee5662eef69fdb1feeb8f48d2a92
NCristofe <cristofegit@gmail.com>
2026-08-25 10:56:03 -0300

    feat: define tela de Cadastro como entrada do app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Button, Image, Pressable, Text, TextInput, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Illustration from '../assets/android-icon-foreground.png'
-import { Card } from './components/Card';
 import { Login } from './screens/Login';
+import { Cadastro } from './screens/Cadastro';
 
 export default function App() {
   return (
     <SafeAreaView>
-      <Login/>     
+      <Cadastro/>     
     </SafeAreaView>
   );
 }
