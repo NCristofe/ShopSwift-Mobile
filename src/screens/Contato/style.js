a3b5ae0b0c4fd9412ace215651b8f6f9e41d3d42
NCristofe <cristofegit@gmail.com>
2026-09-22 09:10:20 -0300

    feat: create style.js

---
+++ src/screens/Contato/style.js
@@ -0,0 +1,64 @@
+import Styled from 'styled-components/native';
+
+export const ContainerContact = Styled.View`
+padding: 20px;
+gap: 40px;
+
+`
+export const MapContainer = Styled.TextInput`
+align-items: center;
+gap: 20px;
+
+
+`
+
+export const MapText = Styled.Text`
+fsont-size: 24px;
+font-weight: 700;
+
+`   
+
+export const MapImage = Styled.Image`
+width: 350px;
+onject-fit: cover;
+
+`
+export const ContainerForm = Styled.View`
+gap: 20px;
+    
+`
+export const MessageText = Styled.Text`
+font-size: 24px;
+font-weight: 700;
+align-text: center;
+
+
+`
+
+export const TextLabel = Styled.Text `
+font-size: 16px;
+`
+
+export const TextInput = Styled.TextInput`
+border-width: 1px solid #000;
+
+`
+export const DescriptionInput = Styled.Text`
+border: 1px solid #000;
+font-size: 16px;
+height: 150px;
+`
+export const Button = Styled.Pressable`
+background-color: ;
+padding: 15px 0 ;
+border-radius: 7px;
+
+`
+export const ButtonText = Styled.Text`   
+align-text: center;
+color: #fff;
+font-size: 20px;
+font-weight: 700;
+
+
+`
