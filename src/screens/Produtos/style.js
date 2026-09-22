b3d4f4976c5b280e76b2fd5d6be078c6677fd934
NCristofe <cristofegit@gmail.com>
2026-09-22 09:10:38 -0300

    feat: create style.js

---
+++ src/screens/Produtos/style.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components/native';
+
+export const ContainerProducts = styled.ScrollView`
+
+padding: 0 20px;
+gap: 20px;
+`
+
+export const ContainerSearch = styled.View`
+
+flex-direction: row;
+gap: 10px;
+align-items: center;
+
+`
+
+export const ProductSection = styled.View`
+
+flex-direction: row;
+gap: 20px;
+align-items: center;
+
+`
+export const TextInput = styled.TextInput`
+
+border-width: 1px solid #000;
+border-radius: 7px;
+padding: 10px;
+
+
+
+`
+
+
